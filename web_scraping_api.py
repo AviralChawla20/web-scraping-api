4960f4586f5ab683efdfcfaa767b5102b7249e39
AviralChawla20 <aviralchawla02@gmail.com.com>
2023-11-02 21:30:58 +0530

    major_update

--- web_scraping_api.py
+++ web_scraping_api.py
@@ -1,28 +1,40 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import json
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options  # Import Chrome options
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import time
 
 app = Flask(__name__)
 CORS(app)
 
 chrome_binary_path = "/opt/render/project/.render/chrome/opt/google/chrome/chrome"
 
-
-# Function to get the top-most hackathon name and logo
-def get_top_hackathon_name_and_logo(url, link):
+# Function to set up a Chrome driver
+def setup_driver():
     options = Options()
     options.headless = True
     options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
-    options.binary_location = chrome_binary_path  # Set the path to the Chrome binary
-    driver = webdriver.Chrome(options=options)  # Use Chrome driver here
+    options.binary_location = chrome_binary_path
+    driver = webdriver.Chrome(options=options)
+    return driver
 
+# Function to safely extract text from an element
+def extract_text(element):
+    return element.text.strip() if element else "N/A"
+
+# Function to safely extract an attribute from an element
+def extract_attribute(element, attribute):
+    return element.get(attribute) if element else None
+
+# Function to get the top-most hackathon name and logo
+def get_top_hackathon_name_and_logo(url, link):
+    driver = setup_driver()
     try:
         driver.get(url)
+        time.sleep(2)  # Add a delay to ensure page loads properly
+
         page_html = driver.page_source
         soup = BeautifulSoup(page_html, 'html.parser')
 
@@ -30,19 +42,9 @@ def get_top_hackathon_name_and_logo(url, link):
         top_hackathon = soup.find('div', class_='single_profile')
         hackathon_info = {}
 
-        if top_hackathon:
-            # Extract competition name
-            competition_name = top_hackathon.find('h2')
-            if competition_name:
-                hackathon_info['name'] = competition_name.text.strip()
-
-            # Extract logo URL
-            logo_img = top_hackathon.find('div', class_='img').find('img')
-            if logo_img:
-                hackathon_info['logo_url'] = logo_img['src']
-
-            # Add the link parameter
-            hackathon_info['link'] = link
+        hackathon_info['name'] = extract_text(top_hackathon.find('h2'))
+        hackathon_info['logo_url'] = extract_attribute(top_hackathon.find('div', class_='img').find('img'), 'src')
+        hackathon_info['link'] = link
 
         return hackathon_info
 
@@ -51,15 +53,11 @@ def get_top_hackathon_name_and_logo(url, link):
 
 # Function to scrape competition names and redirection links
 def scrape_competitions_list(url, num_competitions):
-    options = Options()
-    options.headless = True
-    options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
-    options.binary_location = chrome_binary_path  # Set the path to the Chrome binary
-    driver = webdriver.Chrome(options=options)  # Use Chrome driver here
-
+    driver = setup_driver()
     try:
         driver.get(url)
+        time.sleep(2)  # Add a delay to ensure page loads properly
+
         page_html = driver.page_source
         soup = BeautifulSoup(page_html, 'html.parser')
 
@@ -68,71 +66,54 @@ def scrape_competitions_list(url, num_competitions):
         # Find all competition listings
         competition_links = soup.find_all('a', class_='Link__LinkBase-sc-af40de1d-0 lkflLS')
 
-        processed_competitions = set()  # To keep track of processed competition names
+        processed_competitions = set()
 
         for link in competition_links:
             competition_name = link.find('h3').text
             competition_link = link['href']
 
-            # Skip if the competition has already been processed
-            if competition_name in processed_competitions:
-                continue
+            if competition_name not in processed_competitions:
+                try:
+                    logo_url = get_competition_logo(competition_link)
+                    competition_list.append({'name': competition_name, 'link': competition_link, 'logo_url': logo_url})
+                    processed_competitions.add(competition_name)
 
-            try:
-                # Visit the redirection link and extract the logo
-                logo_url = get_competition_logo(competition_link)
-            except Exception as e:
-                print(f"Error getting logo for {competition_link}: {e}")
-                logo_url = None  # Set logo_url to None in case of an error
+                    if len(competition_list) >= num_competitions:
+                        break
 
-            competition_list.append({'name': competition_name, 'link': competition_link, 'logo_url': logo_url})
-
-            # Mark the competition as processed
-            processed_competitions.add(competition_name)
-
-            # Stop when we have collected the desired number of competitions
-            if len(competition_list) >= num_competitions:
-                break
+                except Exception as e:
+                    print(f"Error getting logo for {competition_link}: {e}")
 
         return competition_list
-
-    except Exception as e:
-        print(f"Error scraping competitions: {e}")
-        return []  # Return an empty list in case of an error
 
     finally:
         driver.quit()
 
-
 # Function to get the competition logo from the redirected page
 def get_competition_logo(competition_link):
-    options = Options()
-    options.headless = True
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.binary_location = chrome_binary_path  # Set the path to the Chrome binary
-    driver = webdriver.Chrome(options=options)  # Use Chrome driver here
-
+    driver = setup_driver()
     try:
         driver.get(competition_link)
+        time.sleep(2)  # Add a delay to ensure page loads properly
+
         page_html = driver.page_source
         soup = BeautifulSoup(page_html, 'html.parser')
 
         # Extract logo URL from the redirected page
         logo_img = soup.find('img', src=True)
-        if logo_img:
-            logo_url = urljoin(competition_link, logo_img['src'])
-            return logo_url
+        logo_url = urljoin(competition_link, extract_attribute(logo_img, 'src'))
+        return logo_url
 
     except Exception as e:
         print(f"Error getting logo for {competition_link}: {e}")
+        return None
 
     finally:
         driver.quit()
 
 # Define a single API endpoint for combined data
-@app.route('/api/combined_data', methods=['GET'])
-def get_combined_data():
+@app.route('/api/data', methods=['GET'])
+def get_data():
     current_link = 'https://unstop.com/hackathons?oppstatus=recent'
     devfolio_link = 'https://devfolio.co/hackathons'
     num_competitions = 6
@@ -149,15 +130,14 @@ def get_combined_data():
     # Create a list of dictionaries in the desired format
     result = [
         {
-            "name": item.get("name", "N/A"),  # Use "N/A" if 'name' is not present
-            "logo_url": item.get("logo_url", "N/A"),  # Use "N/A" if 'logo_url' is not present
-            "link": item.get("link", "N/A"),  # Use "N/A" if 'link' is not present
+            "name": item['name'],
+            "logo_url": item['logo_url'],
+            "link": item['link'],
         }
         for item in competition_data
     ]
 
     return jsonify(result)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
